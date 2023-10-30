fe8c441d786f29a59e35c3596f475f0f8de2f509
593413198 <593413198@qq.com>
2023-10-31 01:45:39 +0800

    Mod: build nova vsproj

--- build.bat
+++
@@ -1,6 +0,0 @@
-
-cmake . -B build
-
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /m build\Game.vcxproj
-
-.\build\Release\Game.exe
---
+++ build_nova.bat
@@ -0,0 +1,5 @@
+
+cmake . -B build
+
+@REM "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /m build\Game.vcxproj
+@REM .\build\Release\Game.exe
