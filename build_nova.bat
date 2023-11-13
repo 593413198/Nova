a6c56a747a410ead5f8f3f9918139b038dc9c4ba
luhao <593413198@qq.com>
2023-11-13 21:37:09 +0800

    Add: 最小化运行窗口； 1. 抽象 Windows 基类，目前只实现 PC 版本； 2. build_nova.bat 增加 pause

--- build_nova.bat
+++ build_nova.bat
@@ -3,3 +3,5 @@ cmake . -B build
 
 @REM "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /m build\Game.vcxproj
 @REM .\build\Release\Game.exe
+
+pause
--- game/Application.cpp
+++ game/Application.cpp
@@ -2,14 +2,23 @@
 // Luhao 2023/10/31
 
 #include "../engine/Log.h"
+#include "../engine/Platform/Window.h"
 #include "glad/glad.h"
+#include <GLFW/glfw3.h>
 
 int main()
 {
 	Noval::Log::Init();
-	LOG_TRACE("test trace log");
-	LOG_INFO("test info log");
-	LOG_WARN("test warn log");
-	LOG_ERROR("test error log");
+	LOG_INFO("Application Begin:");
+
+	auto window = std::unique_ptr<Nova::Window>(Nova::Window::Create());
+
+	while (1)
+	{
+		glClearColor(0.3, 0.3, 0.3, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		window->OnUpdate();
+	}
+
     return 1;
 }
